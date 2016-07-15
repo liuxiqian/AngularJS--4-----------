fad87b70a99a23eab17d83fa66ef020972b4b0f3
liuxiqian <1454179583@qq.com>
2016-07-15 10:13:23 +0800

    简单的路由

--- app/UIRoute1.js
+++ app/UIRoute1.js
@@ -1,25 +1,26 @@
 var myUIRoute = angular.module('MyUIRoute', ['ui.router', 'ngAnimate']);
+myUIRoute.controller("asds",function($scope){
+ $scope.items = ["A", "List", "Of", "Items"];
+ alert("1");
+})
 myUIRoute.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise("/state1");
     $stateProvider
         .state('state1', {
             url: "/state1",
-            templateUrl: "tpls/state1.html"
+            templateUrl: "tpls1/state1.html"
         })
         .state('state1.list', {
             url: "/list",
-            templateUrl: "tpls/state1.list.html",
-            controller: function($scope) {
-                $scope.items = ["A", "List", "Of", "Items"];
-            }
+            templateUrl: "tpls1/state1.list.html",
         })
         .state('state2', {
             url: "/state2",
-            templateUrl: "tpls/state2.html"
+            templateUrl: "tpls1/state2.html"
         })
         .state('state2.list', {
             url: "/list",
-            templateUrl: "tpls/state2.list.html",
+            templateUrl: "tpls1/state2.list.html",
             controller: function($scope) {
                 $scope.things = ["A", "Set", "Of", "Things"];
             }
